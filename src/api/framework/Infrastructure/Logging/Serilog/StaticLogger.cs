62b0b38d20195d3a69cca2d877aa81bf2a1308ec
M.F.M Fazrin <mfmfazrin1986@gmail.com>
2024-08-27 14:08:47 +0300

    Initial commit

---
+++ src/api/framework/Infrastructure/Logging/Serilog/StaticLogger.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+using Serilog.Core;
+using Serilog.OpenTelemetry;
+using Serilog.Sinks.OpenTelemetry;
+
+namespace FSH.Framework.Infrastructure.Logging.Serilog;
+public static class StaticLogger
+{
+    public static void EnsureInitialized()
+    {
+        if (Log.Logger is not Logger)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.OpenTelemetry()
+                .CreateLogger();
+        }
+    }
+}
